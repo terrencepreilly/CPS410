d0acd86d78d1bb3dcfc9377738c2f8d15d975462
justi1jc <justi1jc@cmich.edu>
2017-02-19 21:14:12 -0500

    Added diagrams

--- hw3/assets/ships.py
+++ hw3/assets/ships.py
@@ -7,10 +7,29 @@ class Player(ActorBase, RenderedBase):
         ActorBase.__init__(health=health, weapons=weapons)
         self.controls = controls
 
+    def update(self):
+        pass # Checks for user inputs and moves or fires accordingly.
+
+    def fire(self):
+        pass # Fires the active weapon
+
+    def move(self):
+        pass # Changes position on screen.
+
 class Enemy(ActorBase, RenderedBase):
     """ Enemy ship that combats player. """
     def __init__(self, health=0, weapons=list()):
         ActorBase.__init__(health=health, weapons=weapons)
+
+    def update(self):
+        pass # Moves downward and randomly either left or right.
+
+    def fire(self):
+        pass # Fires active weapon
+
+    def move(self):
+        pass # Changes position on screen.
+
 
 class LaserBlaster(object):
     """ A weapon for ships to use. """
